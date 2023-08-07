169a2d57447eb8abdf67e34bcba5e39b5eebf417
whale <2786435349@qq.com>
2023-08-07 17:55:14 +0800

    syu

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -16,4 +16,31 @@ export default defineConfig({
       'mermaid': 'mermaid/dist/mermaid.esm.mjs',
     },
   },
+  build: {
+    // outDir: BUILD_DIR,
+    sourcemap: false,
+    minify: 'terser',
+    chunkSizeWarningLimit: 1500,
+    emptyOutDir: true,
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true
+      }
+    },
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        },
+        chunkFileNames: (chunkInfo) => {
+          const facadeModuleId = chunkInfo.facadeModuleId ? chunkInfo.facadeModuleId.split('/') : [];
+          const fileName = facadeModuleId[facadeModuleId.length - 2] || '[name]';
+          return `js/${fileName}/[name].[hash].js`;
+        }
+      }
+    }
+  },
 });
